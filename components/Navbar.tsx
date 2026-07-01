7a84f5afc4cc7aee3afd691e3e97f971b1e205aa
Sadik Saifi <sadiksaifi7428@gmail.com>
2026-07-01 16:27:26 +0530

    feat: initial setup with Next.js, TypeScript, shadcn/ui, folder structure and Navbar component

---
+++ components/Navbar.tsx
@@ -0,0 +1,63 @@
+"use client";
+import Link from "next/link";
+import { useState } from "react";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import { Button } from "./ui/button";
+import { Menu } from "lucide-react";
+import { NavLink } from "@/lib/types";
+
+const navlinks: NavLink[] = [
+  { label: "Home", href: "/" },
+  { label: "About", href: "/about" },
+  { label: "Services", href: "/services" },
+  { label: "Contact", href: "/contact" },
+];
+import React from "react";
+
+function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <nav className="w-full border-b px-6 py-4 flex items-center justify-between">
+      <Link href="/" className="text-xl font-bold">
+        Nova<span>Tech</span>
+      </Link>
+      <div className="hidden md:flex gap-6">
+        {navlinks.map((item) => (
+          <Link
+            href={item.href}
+            key={item.href}
+            className="text-sm hover: underline"
+          >
+            {item.label}
+          </Link>
+        ))}
+      </div>
+      <div className="md:hidden">
+        <Sheet open={isOpen} onOpenChange={setIsOpen}>
+          <SheetTrigger asChild>
+            <Button variant="ghost" size="icon">
+              <Menu className="h-5 w-5"></Menu>
+            </Button>
+          </SheetTrigger>
+          <SheetContent side="right">
+            <div className="flex flex-col gap-6 mt-8">
+              {navlinks.map((item) => (
+                <Link
+                  href={item.href}
+                  key={item.href}
+                  className="text-lg"
+                  onClick={() => setIsOpen(false)}
+                >
+                  {item.label}
+                </Link>
+              ))}
+            </div>
+          </SheetContent>
+        </Sheet>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ lib/types.ts
@@ -0,0 +1,16 @@
+export type NavLink = {
+  label: string;
+  href: string;
+};
+
+export type Service = {
+  title: string;
+  desc: string;
+  icon: string;
+};
+
+export type TeamMember = {
+  name: string;
+  role: string;
+  image: string;
+};
